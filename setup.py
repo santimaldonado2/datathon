c07391c66d850edfcbd3693dd5dc91d4f2b84080
Santiago Maldonado <santimaldonado2@gmail.com>
2020-03-04 22:52:31 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repo for the Cajamar Datathon',
+    author='maldo',
+    license='MIT',
+)
